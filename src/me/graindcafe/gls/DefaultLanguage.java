2765a4b23218c28dbeb0ed28cbc9e70c7d94526e
Graindcafe <killerdead.x666@gmail.com>
2011-08-09 16:03:01 +0200

    Now fully available, improved initialization

--- src/me/graindcafe/gls/DefaultLanguage.java
+++ src/me/graindcafe/gls/DefaultLanguage.java
@@ -4,54 +4,67 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Stack;
-import java.util.logging.Logger;
-
-import org.bukkit.ChatColor;
 import org.bukkit.util.config.Configuration;
 
 public class DefaultLanguage extends Language {
+
 	/**
 	 * Your language version, use it as you want.
 	 */
-	public static final byte Version = 1;
+	public static byte version = 1;
 	/**
 	 * You
 	 */
-	private static final String author = "Graindcafe";
+	private static String author = "Graindcafe";
+	/**
+	 * The default language folder. DON'T FORGET THE LAST SLASH
+	 */
+	protected static String languagesFolder = "plugins/";
 	/**
 	 * This language name
 	 */
-	protected static final String languageName="english";
+	protected static String languageName = "english";
 	/**
 	 * The default sentences
 	 */
 	private static HashMap<String, String> Strings = new HashMap<String, String>() {
-		private static final long serialVersionUID = 9166935722459443352L;
+		private static final long serialVersionUID = 7261634788355469790L;
+
 		{
+			put("File.TheDefaultLanguageFile",
+					"# This is the plugin default language file \n# You should not edit it ! All changes will be undone !\n# Create another language file (custom.yml) \n# and put 'Default: english' if your default language is english\n");
 			put("File.DefaultLanguageFile",
 					"# This is your default language file \n# You should not edit it !\n# Create another language file (custom.yml) \n# and put 'Default: english' if your default language is english\n");
-			put("File.LanguageFileComplete", "# Your language file is complete\n");
-			put("File.TranslationsToDo", "# Translations to do in this language file\n");
+			put("File.LanguageFileComplete",
+					"# Your language file is complete\n");
+			put("File.TranslationsToDo",
+					"# Translations to do in this language file\n");
 			put("Info.ChosenLanguage", "Chosen language : %s. Provided by %s.");
-			put("Warning.LanguageFileMissing", "The specified language file is missing!");
+			put("Warning.LanguageFileMissing",
+					"The specified language file is missing!");
 			put("Warning.LanguageOutdated", "Your language file is outdated!");
-			put("Message.AnExample", ChatColor.GREEN + "This is a green example!.");
-			put("Message.AnotherExample", ChatColor.RED + "This is another example, that is red");
 		}
 	};
+
 	/**
-	 * Check if a language contains all sentences that the Default has.
-	 * If not, it add the missing ones in head of the language file 
-	 * @param l Language to check
+	 * Check if a language contains all sentences that the Default has. If not,
+	 * it add the missing ones in head of the language file
+	 * 
+	 * @param l
+	 *            Language to check
 	 * @return if the language was complete
 	 */
 	public static boolean checkLanguage(Language l) {
+		if (l == null)
+			return false;
 		while (!l.isDefault())
 			l = l.getDefault();
 		boolean retour = true;
 		Configuration lFile = l.getFile();
 		String value;
 		Stack<String> todo = new Stack<String>();
+		if (lFile == null)
+			return false;
 		for (String key : Strings.keySet()) {
 			value = lFile.getString(key, null);
 			if (value == null) {
@@ -59,7 +72,7 @@ public class DefaultLanguage extends Language {
 			}
 		}
 		String header = l.get("File.DefaultLanguageFile");
-		if (lFile.getInt("Version", 0) != DefaultLanguage.Version) {
+		if (lFile.getInt("Version", 0) != DefaultLanguage.version) {
 			retour = false;
 			header += "# " + Strings.get("Warning.LanguageFileOutdated") + "\n";
 		}
@@ -70,31 +83,38 @@ public class DefaultLanguage extends Language {
 			header += l.get("File.TranslationsToDo");
 		}
 		while (!todo.isEmpty())
-			header += "# - " + todo.peek() + ": " + Strings.get(todo.pop()) + "\n";
+			header += "# - " + todo.peek() + ": " + Strings.get(todo.pop())
+					+ "\n";
 		lFile.setHeader(header);
 		lFile.save();
 		return retour;
 	}
+
 	/**
 	 * Save the default language to a YAML file
-	 * @param fileName the name of this language 
+	 * 
+	 * @param fileName
+	 *            the name of this language
 	 */
 	public static void saveDefaultLanguage(String fileName) {
-		File dir = new File(Language.languagesFolder);
+		File dir = new File(DefaultLanguage.languagesFolder);
 		if (!dir.exists()) {
-			String[] languageFolders = Language.languagesFolder.split("/");
+			String[] languageFolders = DefaultLanguage.languagesFolder
+					.split("/");
 			String tmplevelFolder = "";
 			for (byte i = 0; i < languageFolders.length; i++) {
-				tmplevelFolder = tmplevelFolder.concat(languageFolders[i] + java.io.File.separatorChar);
+				tmplevelFolder = tmplevelFolder.concat(languageFolders[i]
+						+ java.io.File.separatorChar);
 				dir = new File(tmplevelFolder);
 				dir.mkdir();
 			}
 		}
-		if (fileName.substring(fileName.length() - 3) != "yml")
+		if (!fileName.substring(fileName.length() - 3).equalsIgnoreCase("yml"))
 			fileName += ".yml";
-		Configuration f = new Configuration(new File(Language.languagesFolder + fileName));
+		Configuration f = new Configuration(new File(
+				DefaultLanguage.languagesFolder + fileName.toLowerCase()));
 		f.setProperty("Author", DefaultLanguage.author);
-		f.setProperty("Version", DefaultLanguage.Version);
+		f.setProperty("Version", DefaultLanguage.version);
 		for (Entry<String, String> e : Strings.entrySet()) {
 			f.setProperty(e.getKey(), e.getValue());
 		}
@@ -102,9 +122,83 @@ public class DefaultLanguage extends Language {
 		f.setHeader("# This is the plugin default language file \n# You should not edit it ! All changes will be undone !\n# Create another language file (custom.yml) \n# and put 'Default: english' if your default language is english\n");
 		f.save();
 	}
-	
-	
-	
+
+	/**
+	 * Set the author of the default language
+	 * 
+	 * @param name
+	 *            : his name
+	 */
+	public static void setAuthor(String name) {
+		author = name;
+	}
+
+	/**
+	 * Set the default language.
+	 * 
+	 * @param locales
+	 *            HashMap<String,String>
+	 */
+	public static void setLocales(HashMap<String, String> locales) {
+		if (locales.containsKey("File.TheDefaultLanguageFile"))
+			locales.put(
+					"File.TheDefaultLanguageFile",
+					"# This is the plugin default language file \n# You should not edit it ! All changes will be undone !\n# Create another language file (custom.yml) \n# and put 'Default: english' if your default language is english\n");
+		if (locales.containsKey("File.DefaultLanguageFile"))
+			locales.put(
+					"File.DefaultLanguageFile",
+					"# This is your default language file \n# You should not edit it !\n# Create another language file (custom.yml) \n# and put 'Default: english' if your default language is english\n");
+		if (locales.containsKey("File.TheDefaultLanguageFile"))
+			locales.put("File.TranslationsToDo",
+					"# Your language file is complete\n");
+		if (locales.containsKey("File.TheDefaultLanguageFile"))
+			locales.put("File.TranslationsToDo",
+					"# Translations to do in this language file\n");
+		if (locales.containsKey("Info.ChosenLanguage"))
+			locales.put("Info.ChosenLanguage",
+					"Chosen language : %s. Provided by %s.");
+		if (locales
+				.containsKey("Warning.TheDeLanguageFileMissingfaultLanguageFile"))
+			locales.put("Warning.LanguageFileMissing",
+					"The specified language file is missing!");
+		if (locales.containsKey("Warning.LanguageOutdated"))
+			locales.put("Warning.LanguageOutdated",
+					"Your language file is outdated!");
+		Strings = locales;
+	}
+
+	/**
+	 * Set the name of the default language (mostly "English")
+	 * 
+	 * @param name
+	 *            : the name
+	 */
+	public static void setName(String name) {
+		languageName = name;
+	}
+
+	/**
+	 * Set the version of your language file
+	 * 
+	 * @param version
+	 */
+	public static void setVersion(byte version) {
+		DefaultLanguage.version = version;
+	}
+
+	/**
+	 * Set the language folder, should be
+	 * plugin.getDataFolder().getPath()+"/languages/" =
+	 * plugins/YOURPLUGIN/languages/
+	 * 
+	 * @param languagesFolder
+	 */
+	public static void setLanguagesFolder(String languagesFolder) {
+		if (!languagesFolder.endsWith("/"))
+			languagesFolder += "/";
+		DefaultLanguage.languagesFolder = languagesFolder;
+	}
+
 	@Override
 	public String get(String key) {
 		return Strings.get(key);
@@ -117,6 +211,6 @@ public class DefaultLanguage extends Language {
 
 	@Override
 	public byte getVersion() {
-		return DefaultLanguage.Version;
+		return DefaultLanguage.version;
 	}
 }
--- src/me/graindcafe/gls/Language.java
+++ src/me/graindcafe/gls/Language.java
@@ -12,41 +12,51 @@ import org.bukkit.util.config.Configuration;
 public class Language {
 
 	/**
-	 * Initialize a language and return it. Check if the given language is correct. Save the default language file
-	 * @param log the Minecraft Logger
-	 * @param languageName the name of the language to load
+	 * Initialize a language and return it. Check if the given language is
+	 * correct. Save the default language file.
+	 * Display the chosen language and his authors
+	 * 
+	 * @param log
+	 *            the Minecraft Logger
+	 * @param languageName
+	 *            the name of the language to load
 	 * @return The language or the default language
 	 */
-	public static Language initLanguage(Logger log, String languageName)
-	{
-		Language Language;
+	public static Language init(Logger log, String languageName) {
+		Language lang;
 		// Save your default language
 		// Should be first in case another language use it.
 		DefaultLanguage.saveDefaultLanguage(DefaultLanguage.languageName);
 		// Get the set language and load it
-		if (languageName  != DefaultLanguage.languageName) {
-			Language = new Language(languageName);
+		if (languageName != DefaultLanguage.languageName) {
+			lang = new Language(languageName);
 			// check the version
-			if (Language.getVersion() != DefaultLanguage.Version)
-				// 255 is a special return of getVersion, if the file was not loaded
-				if (Language.getVersion() == 255)
-					log.warning(Language.get("Warning.LanguageFileMissing"));
+			if (lang.getVersion() != DefaultLanguage.version)
+				// 255 is a special return of getVersion, if the file was not
+				// loaded
+				if (lang.getVersion() == 255)
+					log.warning(lang.get("Warning.LanguageFileMissing"));
 				else {
-					// Put the missing sentences in the default language file header
-					DefaultLanguage.checkLanguage(Language);
-					log.warning(Language.get("Warning.LanguageFileOutdated"));
+					// Put the missing sentences in the default language file
+					// header
+					DefaultLanguage.checkLanguage(lang);
+					log.warning(lang.get("Warning.LanguageFileOutdated"));
 				}
-			// check if all sentences are in the given language file, and put the missing ones in its header
-			else if (!DefaultLanguage.checkLanguage(Language))
-				log.warning(Language.get("Warning.LanguageFileOutdated"));
+			// check if all sentences are in the given language file, and put
+			// the missing ones in its header
+			else if (!DefaultLanguage.checkLanguage(lang))
+				log.warning(lang.get("Warning.LanguageFileOutdated"));
 		} else {
-			// If it's your defaultlanguage, don't load a configuration and unnecessary stuff
-			Language = new DefaultLanguage();
+			// If it's your defaultlanguage, don't load a configuration and
+			// unnecessary stuff
+			lang = new DefaultLanguage();
 		}
 		// Display the author
-		log.info(String.format(Language.get("Info.ChosenLanguage"), languageName, Language.getAuthor()));
-		return Language;
+		log.info(String.format(lang.get("Info.ChosenLanguage"), lang.getName(),
+				lang.getAuthor()));
+		return lang;
 	}
+
 	/**
 	 * The YAML file corresponding to this language
 	 */
@@ -60,35 +70,53 @@ public class Language {
 	 * Parsed strings
 	 */
 	protected HashMap<String, String> finalStrings;
-	/**
-	 * The default language folder. DON'T FORGET THE LAST SLASH
-	 */
-	protected static String languagesFolder="plugins/YOURPLUGIN/languages/";
+
 	/**
 	 * This language name
 	 */
 	protected String languageName;
+
 	/**
-	 * Useful for the Default Language that doesn't use File, Default and finalStrings
+	 * Useful for the Default Language that doesn't use File, Default and
+	 * finalStrings
 	 */
 	protected Language() {
 
 	}
+
 	/**
 	 * 
-	 * @param LanguageName Should be the language's file name
+	 * @param languageName
+	 *            Should be the language's file name
 	 */
-	public Language(String LanguageName) {
-
-		java.io.File f = new java.io.File(Language.languagesFolder + LanguageName);
+	public Language(String languageName) {
+		if (!languageName.substring(languageName.length() - 3)
+				.equalsIgnoreCase("yml"))
+			languageName += ".yml";
+		// try to get the file as it is given 
+		java.io.File f = new java.io.File(DefaultLanguage.languagesFolder
+				+ languageName);
 		if (!f.exists()) {
-			f = new java.io.File(Language.languagesFolder + LanguageName + ".yml");
-		}
+			// no ? Maybe a problem of cases
+			java.io.File dir = new java.io.File(DefaultLanguage.languagesFolder);
+			if (dir.exists()) {
+				// List all files and try to get one corresponding with
+				// languageName
+				for (java.io.File file : dir.listFiles()) {
+					if (file.getName().equalsIgnoreCase(languageName)) {
+						f = file;
+						break;
+					}
+				}
+			}
 
+		}
+		// strip the extension
+		languageName = languageName.substring(0,languageName.length() - 4);
 		if (f.exists()) {
 			File = new Configuration(f);
 			File.load();
-
+			this.languageName = File.getString("Name", languageName);
 			if (File.getString("Default", null) != null) {
 				Default = new Language(File.getString("Default"));
 			} else {
@@ -97,14 +125,17 @@ public class Language {
 		} else {
 			File = null;
 			Default = new DefaultLanguage();
+			this.languageName = languageName;
 		}
 		finalStrings = new HashMap<String, String>();
-		languageName=File.getString("Name",LanguageName);
 
 	}
+
 	/**
 	 * Get a locale specified by the giving key
-	 * @param key The key of the sentence you want to have
+	 * 
+	 * @param key
+	 *            The key of the sentence you want to have
 	 * @return The sentence or null if none of default languages have it.
 	 */
 	public String get(String key) {
@@ -124,58 +155,77 @@ public class Language {
 			return finalString;
 		}
 	}
+
 	/**
 	 * Get all contributors for this language
+	 * 
 	 * @return Authors separated by comma
 	 */
 	public String getAuthor() {
 		if (File != null)
-			return File.getString("Author", "Anonymous") + (Default instanceof DefaultLanguage ? "" : ", " + Default.getAuthor());
+			return File.getString("Author", "Anonymous")
+					+ (Default instanceof DefaultLanguage ? "" : ", "
+							+ Default.getAuthor());
 		else
 			return Default.getAuthor();
 	}
+
 	/**
-	 * Get the default language 
+	 * Get the default language
+	 * 
 	 * @return Default Language for this language
 	 */
 	protected Language getDefault() {
 		return Default;
 	}
+
 	/**
 	 * Get the file as a Configuration object
+	 * 
 	 * @return Configuration
 	 */
 	protected Configuration getFile() {
 		return File;
 	}
+
 	/**
 	 * Get the language name
+	 * 
 	 * @return Language name
 	 */
-	public String getName()
-	{
+	public String getName() {
 		return languageName;
 	}
+
 	/**
 	 * Get the version or 255 if the file is missing
+	 * 
 	 * @return Version or 255 if the file is missing
 	 */
 	public byte getVersion() {
 		if (File != null)
-			return (byte) File.getInt("Version", Default instanceof DefaultLanguage ? 0 : Default.getVersion());
+			return (byte) File.getInt(
+					"Version",
+					Default instanceof DefaultLanguage ? 0 : Default
+							.getVersion());
 		else
 			return (byte) 255;
 	}
+
 	/**
 	 * Is this language a root ?
+	 * 
 	 * @return true if this language is not completing/modifying another one.
 	 */
 	protected boolean isDefault() {
 		return Default instanceof DefaultLanguage;
 	}
+
 	/**
-	 * Replace &x in §x so that colors appear in game 
-	 * @param s The string to parse
+	 * Replace &x in §x so that colors appear in game
+	 * 
+	 * @param s
+	 *            The string to parse
 	 * @return The parsed string
 	 */
 	public String parseColor(String s) {
